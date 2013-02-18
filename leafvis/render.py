0f7650ed11f2d27756b09e4244b4cc11240f2e57
Nathan Faggian <nathan.faggian@gmail.com>
2013-02-18 14:14:38 +1100

    Updates tests

--- leafvis/render.py
+++ leafvis/render.py
@@ -1,21 +1,23 @@
 """
-Rendering module
+Rendering module, responsible for *fast* png creation.
 """
+
+import StringIO
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-def draw_tile(tl, br):
+def draw_tile(grid, cmap='gray', dpi=80, alpha=0.5, vim=0, vmax=2500):
 
-    grid = resample(tl, br)
-
-    fig = Figure(dpi=80, edgecolor='none')
-    fig.patch.set_alpha(0.1)
+    fig = Figure(dpi=dpi, edgecolor='none')
+    
+    # Form the graphic
     ax = fig.add_axes((0,0,1,1))
-    ax.matshow(grid, interpolation='nearest', alpha=0.5, vmin=0, vmax=2500)
-    ax.contour(grid, level=[500])
+    ax.matshow(grid, interpolation='nearest', alpha=alpha)
     ax.axis('off')
     ax.axis('tight')
+
+    # Write the PNG file to a string.
     canvas = FigureCanvas(fig)
     png_output = StringIO.StringIO()
     canvas.print_png(png_output, transparent=True)
